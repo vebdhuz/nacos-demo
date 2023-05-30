9dc9b6f4d2f833967082ae2e3fd84d5e898f7f29
chenyouxiang <chenyouxiang007@qq.com>
2023-05-30 20:30:55 +0800

    add

--- server/src/main/java/com/example/server/controller/HelloController.java
+++ server/src/main/java/com/example/server/controller/HelloController.java
@@ -4,10 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+
 @RestController
 public class HelloController {
   
   @GetMapping(value="/hello/{str}")
+  @SentinelResource("hello")
   public String Hello(@PathVariable String str) {
     return "hello:"+str;
   }
